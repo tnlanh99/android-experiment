8a03b79c5c2585d013ccbde5385787ebf909e989
tnlanh99 <tnlanh99@gmail.com>
2023-05-05 02:14:46 +0700

    chore: update dependencies and plugins versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import java.util.Locale
 
 plugins {
-    id("com.android.application") version "8.0.0" apply false
-    id("com.android.library") version "8.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
-    id("com.diffplug.spotless") version "6.18.0"
+    id("com.android.application") version "8.0.2" apply false
+    id("com.android.library") version "8.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("com.diffplug.spotless") version "6.19.0"
     id("com.github.ben-manes.versions") version "0.46.0"
 }
 
